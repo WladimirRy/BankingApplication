9619f70f8c11236c4c6a4be847429e22a0afbe67
Wladimir_Ru <n.zero2012@yandex.ru>
2025-06-30 00:58:33 +0300

    4й коммит(общая функиция счета и карт клиентов)

--- src/widget.py
+++ src/widget.py
@@ -1,3 +1,6 @@
+from src.masks import get_mask_account, get_mask_card_number
+
+
 def get_date(s: str) -> str:
     """Функция принимает на вход строку с датой
     в формате "2024-03-11T02:26:18.671407" и
@@ -5,3 +8,20 @@ def get_date(s: str) -> str:
     формате "ДД.ММ.ГГГГ. ("11.03.2024")"""
     result = f"{s[8:10]}.{s[5:7]}.{s[0:4]}"
     return result
+
+
+def mask_account_card(s: str) -> str:
+    """Функция принимает на вход номер карты или счета и
+    выводит информацию в форматах:
+    ('ТИП КАРТЫ' XXXX XX** **** XXXX) - для карты,
+    ('СЧЁТ' **XXXX) - для счёта."""
+    digital_value = 0
+    for i in s:
+        if i.isdigit():
+            digital_value += 1
+    if digital_value >= 17:
+        result = get_mask_account(s)
+        return result
+    else:
+        result = get_mask_card_number(s)
+        return result
--- tests/testing.py
+++ tests/testing.py
@@ -1,5 +1,5 @@
 from src.masks import get_mask_account, get_mask_card_number
-from src.widget import get_date
+from src.widget import get_date, mask_account_card
 
 
 card_client_input = str(input("Enter card numder (str):"))
@@ -12,3 +12,7 @@ print(get_mask_account(score_client_input))
 
 date_input = str(input("Enter date (str):"))
 print(get_date(date_input))
+
+
+client_input = str(input("Enter client (str):"))
+print(mask_account_card(client_input))
