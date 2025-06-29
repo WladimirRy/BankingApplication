523e9b5b1fb4480d2383498133ed32050267b444
Wladimir_Ru <n.zero2012@yandex.ru>
2025-06-30 00:11:27 +0300

    3й коммит(добовление функции даты)

--- tests/testing.py
+++ tests/testing.py
@@ -1,4 +1,6 @@
 from src.masks import get_mask_account, get_mask_card_number
+from src.widget import get_date
+
 
 card_client_input = str(input("Enter card numder (str):"))
 print(get_mask_card_number(card_client_input))
@@ -6,3 +8,7 @@ print(get_mask_card_number(card_client_input))
 
 score_client_input = str(input("Enter score numder (str):"))
 print(get_mask_account(score_client_input))
+
+
+date_input = str(input("Enter date (str):"))
+print(get_date(date_input))
