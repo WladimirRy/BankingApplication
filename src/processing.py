b92a9fd11765ab5866b1b836d6cfac8464d33ccd
Wladimir_Ru <n.zero2012@yandex.ru>
2025-08-13 23:32:16 +0300

    8й коммит(редактирование processing.py)

--- src/processing.py
+++ src/processing.py
@@ -8,7 +8,7 @@ def filter_by_state(state_list: list, state: str = "EXECUTED") -> list:
             if state_list[i]["state"] == "EXECUTED":
                 new_state.append(state_list[i])
         return new_state
-    elif state == "CANCELED":
+    else:
         for i in range(len(state_list)):
             if state_list[i]["state"] == "CANCELED":
                 new_state.append(state_list[i])
@@ -16,7 +16,8 @@ def filter_by_state(state_list: list, state: str = "EXECUTED") -> list:
 
 
 def sort_by_date(state_list: list, direction_sort: bool = True) -> list:
-    """Функция принимает список словарей и необязательный параметр, задающий порядок сортировки (по умолчанию — убывание),
-    т. е. сначала самые последние операции. Функция возвращает новый список, отсортированный по дате('date'). """
-    new_data_list = sorted(state_list, key=lambda state_lists: state_lists["date"], reverse=direction_sort)
-    return new_data_list
+    """Функция принимает список словарей и необязательный параметр, задающий порядок сортировки
+    (по умолчанию — убывание), т. е. сначала самые последние операции. Функция возвращает новый список,
+     отсортированный по дате('date'). """
+    new_date = sorted(state_list, key=lambda state_lists: state_lists["date"], reverse=direction_sort)
+    return new_date
